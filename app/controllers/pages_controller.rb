ae8a90632b24e4592ff09bfefa83fe9597586248
Kyle Geske <stungeye@gmail.com>
2014-11-05 11:02:18 -0600

    Pre Pages model revamp with not ContentPage model.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,5 @@ class PagesController < ApplicationController
     
   end
   
-  def about_us
-  end
-  # Next, the associated view is automatically loaded.
-  # app/views/pages/about_us.html.erb
-
-  def contact_us
-  end
-  # Automatically loads app/views/pages/contact_us.html.erb
   
-  def wacky_wednesday
-  end
 end
